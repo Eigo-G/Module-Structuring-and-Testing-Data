851863a83e4708d0d20b397d460c8824ba4fcade
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-03-16 15:49:17 +0100

    Created a new branch, answered the question in 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 is updating the value of the count variable by adding 1 to its current value. The = operator is an assignment operator that assigns the result of the expression on the right (count + 1) back to the variable on the left (count). So, it takes the current value of count, adds 1 to it, and then stores that new value back in count.
