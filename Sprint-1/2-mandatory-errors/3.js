dd35db26779034d40b6bd91db12d7d7c7441bc10
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-03-20 11:54:52 +0100

    numbers do not have .slice converted to .tostring()

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,8 @@
 const cardNumber = 4533787178994213;
 const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(`The last 4 digits of the card number are ${last4Digits}`);
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
