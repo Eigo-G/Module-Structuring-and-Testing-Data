b06c90e04f74e9c87bb63d749ebc4b99b8ee457f
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-03-20 12:11:10 +0100

    switched the variables around

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,13 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+// The 12HourClockTime variable should store the time in 12 hour clock format
+// The 24HourClockTime variable should store the time in 24 hour clock format
+// However, the code isn't working
+// Before running the code, make and explain a prediction about why the code won't work
+// Then run the code and see what error it gives.
+// Consider: Why does it give this error? Is this what I predicted? If not, what's different?
+// Then try updating the variable names to get the correct values stored in the variables
+
+const 24HourClockTime = "20:53";
+const 12HourClockTime = "08:53";
