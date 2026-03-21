3c7cb8724e38f36129a0f9df3442761d46c58fdf
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-03-21 19:40:33 +0100

    mandatory-implement 3

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -1,6 +1,46 @@
 // In Sprint-1, there is a program written in interpret/to-pounds.js
 
-// You will need to take this code and turn it into a reusable block of code.
+// You will need to take this code and turn it into a reusable block of code
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
+const penceString = "399p";
+
+const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1
+);
+
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+console.log(`£${pounds}.${pence}`);
 // You should call this function a number of times to check it works for different inputs
+function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
+
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+
+  return `£${pounds}.${pence}`;
+}
+
+console.log(toPounds("399p")); // This should output "£3.99"
+console.log(toPounds("5p")); // This should output "£0.05"
+console.log(toPounds("1234p")); // This should output "£12.34"
