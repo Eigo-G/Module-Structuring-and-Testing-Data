f02ad7d23c5ba2a5d326d7f639ca26639ad44da1
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-03-21 19:30:30 +0100

    mandatory-implement 2

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,16 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+function toUpperSnakeCase(str) {
+  // Step 1: Convert the string to uppercase
+  const upperCaseStr = str.toUpperCase();
+
+  // Step 2: Replace spaces with underscores
+  const snakeCaseStr = upperCaseStr.replace(/ /g, "_");
+
+  return snakeCaseStr;
+}
+
+// Example usage:
+console.log(toUpperSnakeCase("hello there")); // Output: "HELLO_THERE"
+console.log(toUpperSnakeCase("lord of the rings")); // Output: "LORD_OF_THE_RINGS"
