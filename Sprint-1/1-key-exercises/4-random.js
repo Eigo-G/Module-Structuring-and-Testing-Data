ffc7f8c718999fdfaab10de1135ae2f6a5b2966f
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-03-21 18:03:18 +0100

    changed name of branch

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,17 +2,16 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-console.log(num);  
+console.log(num);
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-} 
+}
 num = getRandomInt(minimum, maximum);
 console.log(num);
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
